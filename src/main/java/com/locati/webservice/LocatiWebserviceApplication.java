add0a95f863f0e999493c19f2d010d364bb10bc2
Bruno Costa <brunohc1991@gmail.com>
2024-09-17 17:38:06 -0300

    first commit

---
+++ src/main/java/com/locati/webservice/LocatiWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.locati.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocatiWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocatiWebserviceApplication.class, args);
+	}
+
+}
