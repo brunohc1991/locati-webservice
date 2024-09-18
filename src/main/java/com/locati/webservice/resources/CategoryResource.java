3682d7adf8416a1762cebd61466abac2453da889
Bruno Costa <brunohc1991@gmail.com>
2024-09-18 09:30:57 -0300

    add first domain class

--- src/main/java/com/locati/webservice/resources/CategoryResource.java
+++ src/main/java/com/locati/webservice/resources/CategoryResource.java
@@ -1,16 +1,33 @@
 package com.locati.webservice.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.locati.webservice.domain.Category;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
 	
 	@GetMapping()
-	public String listAll() {
-		return 	"ok";
+	public ResponseEntity<List<Category>> listAll() {
+		Category cat1 = new Category();
+		cat1.setId(1L);
+		cat1.setName("Informática");
+		Category cat2 = new Category();
+		cat2.setId(2L);
+		cat2.setName("Escritório");
+		
+		List<Category> list = new ArrayList<>();
+		list.add(cat1);
+		list.add(cat2);
+		
+		return 	ResponseEntity.ok(list);
 	}
 
 }
